e6fb8674b5ce78b915e27adb67f5d4b3a0544af7
SunilNeupane77 <sunil.799406@sdc.tu.edu.np>
2024-04-23 14:50:44 +0545

    payment form

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -5,3 +5,7 @@ class UserForm(forms.Form):
    middle_name=forms.CharField(max_length=10,min_length=2,required=False)
    last_name=forms.CharField(max_length=10,min_length=2)
    
+class PayementForm(forms.Form):
+   first_name=forms.CharField(max_length=12,min_length=5)
+   middle_name=forms.EmailField(max_length=45,min_length=12,required=True)  
+   
