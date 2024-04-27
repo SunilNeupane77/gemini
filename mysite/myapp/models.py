387e3cd2b899765b692d5fd2fb3b33fbe025e11a
SunilNeupane77 <sunil.799406@sdc.tu.edu.np>
2024-04-27 22:06:32 +0545

    gender choices added

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 # Create your models here.
 class  Person(models.Model):
+   gender_choices={
+      ("M","Male"),
+      ("F","Female"),
+      ("O","Others"),
+   }
+   
+   
    first_name=models.CharField(verbose_name=("person name"),max_length=10)
    middle_name=models.CharField('middle name',max_length=15,blank=True,null=True)
    last_name=models.CharField("Last name",max_length=15)
    email=models.EmailField("Your email")
+   gender=models.CharField(max_length=1,choices=gender_choices,default="M")
