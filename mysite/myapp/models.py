fb26d104ceb1389cee538b86bc81442afc2549ec
SunilNeupane77 <sunil.799406@sdc.tu.edu.np>
2024-04-27 22:01:45 +0545

    writing model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class  Person(models.Model):
+   first_name=models.CharField(verbose_name=("person name"),max_length=10)
+   middle_name=models.CharField('middle name',max_length=15,blank=True,null=True)
+   last_name=models.CharField("Last name",max_length=15)
+   email=models.EmailField("Your email")
