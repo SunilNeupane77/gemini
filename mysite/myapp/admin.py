c2f0b99c7c6af2fd25d50a9aafc35646d33057c1
SunilNeupane77 <sunil.799406@sdc.tu.edu.np>
2024-05-23 14:55:37 +0545

    vehicles modeladded

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Person
+from .models import Vehicles
 
 # Register your models here.
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
    list_display=["id","first_name"]
+   
+@admin.register(Vehicles)
+ class vehiclesnaem(admin.ModelAdmin):
+    list_display=["model_number","color"]
